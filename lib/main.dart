fecea1e8c8dc51b0b55e3caf4a894fd4c1708b72
kevin2-cyber <73193206+kevin2-cyber@users.noreply.github.com>
2021-11-15 18:30:44 -0800

    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:studi_kasus/login_page.dart';
-import 'splash_screen.dart';
 
 void main() {
   runApp(MyApp());
